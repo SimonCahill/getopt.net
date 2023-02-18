f78196f4a0310a3c8f76d0fb474c1973d00a3d94
Simon Cahill <simon@simonc.eu>
2023-02-18 22:11:45 +0100

    Added first two extension methods

---
+++ getopt.net/Extensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace getopt.net {
+
+	using System.Linq;
+
+	public static class Extensions {
+
+		public static Option? FindOptionOrDefault(this Option[] list, string optName) {
+			foreach (var opt in list) {
+				if (opt.Name?.Equals(optName, StringComparison.InvariantCulture) == true) {
+					return opt;
+				}
+			}
+
+			return null;
+		}
+
+		public static Option? FindOptionOrDefault(this Option[] list, char optVal) {
+			foreach (var opt in list) {
+				if (opt.Value == optVal) {
+					return opt;
+				}
+			}
+
+			return null;
+		}
+
+	}
+}
+

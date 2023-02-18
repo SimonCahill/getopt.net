4febe2e205d8bba88797170c8c2d0e3bceb7b30c
Simon Cahill <simon@simonc.eu>
2023-02-18 22:12:24 +0100

    Added ParseException class

---
+++ getopt.net/ParseException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace getopt.net {
+
+	public class ParseException: Exception {
+		public ParseException(string msg): this(null, msg) { }
+
+		public ParseException(string? optString, string msg): base(msg) {
+			Option = optString;
+		}
+
+		/// <summary>
+		/// The option that caused the exception.
+		/// </summary>
+		public string? Option { get; set; }
+	}
+}
+

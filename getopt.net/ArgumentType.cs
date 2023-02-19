ef2165b8264dd1bd7dd76d2a981cabdae1a7b587
Simon Cahill <simon@simonc.eu>
2023-02-19 01:27:41 +0100

    Visual Studio for Mac has a stupid bug where it ignores my preferences and uses tabs instead of spaces.

--- getopt.net/ArgumentType.cs
+++ getopt.net/ArgumentType.cs
@@ -2,26 +2,26 @@
 
 namespace getopt.net {
 
-	/// <summary>
-	/// Enumeration containing the argument types possible for getopt.
-	/// </summary>
-	public enum ArgumentType {
+    /// <summary>
+    /// Enumeration containing the argument types possible for getopt.
+    /// </summary>
+    public enum ArgumentType {
 
-		/// <summary>
-		/// No argument is required for the option.
-		/// </summary>
-		None,
+        /// <summary>
+        /// No argument is required for the option.
+        /// </summary>
+        None,
 
-		/// <summary>
-		/// The option has a mandatory argument.
-		/// </summary>
-		Required,
+        /// <summary>
+        /// The option has a mandatory argument.
+        /// </summary>
+        Required,
 
-		/// <summary>
-		/// Arguments are optional for this argument.
-		/// </summary>
-		Optional
+        /// <summary>
+        /// Arguments are optional for this argument.
+        /// </summary>
+        Optional
 
-	}
+    }
 }
 

464bf7e80e2f432b6a08cff4302ea3795d1262b0
Simon Cahill <simon@simonc.eu>
2023-02-28 15:04:22 +0100

    Updated code documentation

--- getopt.net/ParseException.cs
+++ getopt.net/ParseException.cs
@@ -2,13 +2,33 @@
 
 namespace getopt.net {
 
+	/// <summary>
+	/// Generic exception class that is thrown when the parser is not configured to ignore errors.
+	/// 
+	/// This exception may be thrown whenever one of the following options is disabled:
+	///  - <see cref="GetOpt.IgnoreEmptyAppArgs" />
+	///  - <see cref="GetOpt.IgnoreEmptyOptions" />
+	///  - <see cref="GetOpt.IgnoreInvalidOptions" />
+	///  - <see cref="GetOpt.IgnoreMissingArgument" />
+	/// </summary>
     public class ParseException: Exception {
+
+		/// <summary>
+		/// Generic exception constructor; only accepts the error message.
+		/// </summary>
+		/// <param name="msg">A brief description of the error.</param>
         public ParseException(string msg): this(null, msg) { }
 
+		/// <summary>
+		/// Specialsied exception constructor; accepts the option causing the error and an accompanying message.
+		/// </summary>
+		/// <param name="optString">The option that caused the exception.</param>
+		/// <param name="msg">A brief description of the error.</param>
         public ParseException(string? optString, string msg): base(msg) {
             Option = optString;
         }
 
+		/// <inheritdoc />
 		public override string ToString() {
 			if (string.IsNullOrEmpty(Option)) {
 				return base.ToString();

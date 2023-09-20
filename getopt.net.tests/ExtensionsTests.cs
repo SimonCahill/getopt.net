56ac181884e2e3ed2203ea76fdf801c35d302483
Simon Cahill <simon@simonc.eu>
2023-09-20 23:44:40 +0200

    Added tests for Extensions

---
+++ getopt.net.tests/ExtensionsTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace getopt.net.tests {
+
+    [TestClass]
+    public class ExtensionsTests {
+
+        [TestMethod]
+        public void TestLeftAlignString_WhitespacePadding() {
+            const string testString = "This is left-aligned";
+            const string desiredString = $"{testString}            ";
+
+            Assert.AreEqual(testString.AlignLeft(desiredString.Length), desiredString);
+        }
+
+        [TestMethod]
+        public void TestLeftAlignString_PlusSignPadding() {
+            const string testString = "This is left-aligned";
+            const string desiredString = $"{testString}++++++++++++";
+
+            Assert.AreEqual(testString.AlignLeft(desiredString.Length, '+'), desiredString);
+        }
+
+        [TestMethod]
+        public void TestLeftAlignString_UmlautPadding() {
+            const string testString = "This is left-aligned";
+            const string desiredString = $"{testString}üüüüüüüüüüüü";
+
+            Assert.AreEqual(testString.AlignLeft(desiredString.Length, 'ü'), desiredString);
+        }
+
+        [TestMethod]
+        public void TestRightAlignString_WhitespacePadding() {
+            const string testString = "This is right-aligned";
+            const string desiredString = $"            {testString}";
+
+            Assert.AreEqual(testString.AlignRight(desiredString.Length), desiredString);
+        }
+
+        [TestMethod]
+        public void TestRightAlignString_PlusSignPadding() {
+            const string testString = "This is right-aligned";
+            const string desiredString = $"++++++++++++{testString}";
+
+            Assert.AreEqual(testString.AlignRight(desiredString.Length, '+'), desiredString);
+        }
+
+        [TestMethod]
+        public void TestRightAlignString_UmlautPadding() {
+            const string testString = "This is right-aligned";
+            const string desiredString = $"üüüüüüüüüüüü{testString}";
+
+            Assert.AreEqual(testString.AlignRight(desiredString.Length, 'ü'), desiredString);
+        }
+
+        [TestMethod]
+        public void TestCentreAlignString_WhitespacePadding() {
+            const string testString = "This is centre-aligned";
+            const string desiredString = $"      {testString}      ";
+
+            Assert.AreEqual(testString.AlignCentre(desiredString.Length), desiredString);
+        }
+
+        [TestMethod]
+        public void TestCentreAlignString_PlusSignPadding() {
+            const string testString = "This is centre-aligned";
+            const string desiredString = $"++++++{testString}++++++";
+
+            Assert.AreEqual(testString.AlignCentre(desiredString.Length, '+'), desiredString);
+        }
+
+        [TestMethod]
+        public void TestCentreAlignString_UmlautPadding() {
+            const string testString = "This is centre-aligned";
+            const string desiredString = $"üüüüüü{testString}üüüüüü";
+
+            Assert.AreEqual(testString.AlignCentre(desiredString.Length, 'ü'), desiredString);
+        }
+
+    }
+}
+

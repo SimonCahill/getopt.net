e2067390c5cbbd5f295be85e10382624ba1c4817
Simon Cahill <simon@simonc.eu>
2023-12-22 19:41:21 +0100

    Added extension method to convert longopt array to shortopt string. Added .net8

---
+++ getopt.net/CommandOption.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace getopt.net {
+
+    /// <summary>
+    /// Represents a single argument received via command-line options.
+    /// </summary>
+    public class CommandOption {
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public CommandOption() { }
+
+        /// <summary>
+        /// Object constructor - instantiates a new instance of this class.
+        /// </summary>
+        /// <param name="optChar">The value of the option passed.</param>
+        /// <param name="optArg">The option's argument (if any)</param>
+        public CommandOption(int optChar, object? optArg = null) {
+            ArgumentType = optArg?.GetType();
+            OptionArgument = optArg;
+        }
+
+        /// <summary>
+        /// The option character that was received.
+        /// </summary>
+        public int OptChar { get; set; }
+
+        /// <summary>
+        /// The type of the argument that was received.
+        /// 
+        /// Possible types are int, string, bool, double
+        /// </summary>
+        public Type? ArgumentType { get; set; }
+
+        /// <summary>
+        /// The actual argument passed to the application.
+        /// </summary>
+        public object? OptionArgument { get; set; }
+
+    }
+}

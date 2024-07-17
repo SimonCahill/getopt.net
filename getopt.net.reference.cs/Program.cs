8a748d17b7f5e81b32bda2993a18c75f7c196f08
Simon Cahill <simon@simonc.eu>
2024-07-17 22:32:52 +0200

    Updated C# reference to use new help text generation. Sorry VB bros

--- getopt.net.reference.cs/Program.cs
+++ getopt.net.reference.cs/Program.cs
@@ -10,9 +10,9 @@ namespace getopt.net.reference.cs {
             var getopt = new GetOpt {
                 AppArgs = args,
                 Options = new[] {
-                    new Option("help",      ArgumentType.None,      'h'),
-                    new Option("version",   ArgumentType.None,      'v'),
-                    new Option("file",      ArgumentType.Required,  'f')
+                    new Option("help",      ArgumentType.None,      'h', "Displays this help text."),
+                    new Option("version",   ArgumentType.None,      'v', "Displays the version of this program."),
+                    new Option("file",      ArgumentType.Required,  'f', "Reads the file back to stdout. The file is read into a local buffer and then printed out. Also I created this really long description to show that getopt.net can handle long descriptions."),
                 },
                 ShortOpts = "hvf:t;", // the last option isn't an error!
                 AllowParamFiles = true,
@@ -26,7 +26,7 @@ namespace getopt.net.reference.cs {
             while ((optChar = getopt.GetNextOpt(out var optArg)) != -1) {
                 switch (optChar) {
                     case 'h':
-                        PrintHelp();
+                        PrintHelp(getopt);
                         return 0;
                     case 'v':
                         PrintVersion();
@@ -55,40 +55,14 @@ namespace getopt.net.reference.cs {
             return 0;
         }
 
-        static void PrintHelp() {
-            Console.WriteLine(
-            """
-            myapp (C#) v1.0.0
-            This app displays the usage of getopt.net in C#.
-
-            Usage:
-                myapp [-h/--help] [-v/--version]
-                myapp -f/path/to/file
-                myapp -f /path/to/file
-                myapp --file=/path/to/file
-                myapp --file /path/to/file
-
-                myapp @/path/to/paramfile # load all args from param file
-            
-            Arguments:
-                --help,     -h      Displays this menu and exits
-                /help,      /h      Displays this menu and exits (Windows conventions)
-                -help,      -h      Displays this menu and exits (Powershell conventions)
-
-                --version,  -v      Displays the version and exits
-                /version,   /v      Displays the version and exits (Windows conventions)
-                -version,   -v      Displays the version and exits (Powershell conventions)
-
-                --file=<>,  -f<>    Reads the file back to stdout.
-                --file <>,  -f<>    Reads the file back to stdout.
-                --file:<>,  -f<>    Reads the file back to stdout. (Windows arg conventions)
-                /file=<>,   /f<>    Reads the file back to stdout. (Windows opt and GNU/POSIX arg conventions)
-                /file <>,   /f<>    Reads the file back to stdout. (Windows opt and GNU/POSIX arg conventions)
-                /file:<>,   /f<>    Reads the file back to stdout. (Windows conventions)
-                -file=<>,   -f<>    Reads the file back to stdout. (Powershell opt and GNU/POSIX arg conventions)
-                -file <>,   -f<>    Reads the file back to stdout. (Powershell opt and GNU/POSIX arg conventions)
-                -file:<>,   -f<>    Reads the file back to stdout. (Powershell opt and Windows arg conventions)
-            """);
+        static void PrintHelp(GetOpt getopt) {
+            Console.WriteLine(getopt.GenerateHelpText(new HelpTextConfig {
+                ApplicationName = "getopt.net reference",
+                ApplicationVersion = "v1.0.0",
+                FooterText = "This is a reference implementation of getopt.net in C#.",
+                OptionConvention = OptionConvention.GnuPosix,
+                ShowSupportedConventions = true
+            }));
         }
 
         static void PrintVersion() {

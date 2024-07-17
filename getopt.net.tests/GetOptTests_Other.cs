9675549ef40b64e98a6eff47baa17018e8f697ca
Simon Cahill <simon@simonc.eu>
2024-07-17 22:48:49 +0200

    Added more tests

--- getopt.net.tests/GetOptTests_Other.cs
+++ getopt.net.tests/GetOptTests_Other.cs
@@ -63,6 +63,7 @@ namespace getopt.net.tests {
             Assert.IsFalse(getopt.IgnoreInvalidOptions);
         }
 
+        [TestMethod]
         public void TestHelpGeneration() {
             var getopt = new GetOpt {
                 Options = new Option[] {
@@ -72,10 +73,128 @@ namespace getopt.net.tests {
             };
 
             var helpText = getopt.GenerateHelpText();
-            Console.WriteLine(helpText);
             Assert.IsNotNull(helpText);
             Assert.IsTrue(helpText.Contains("help"));
             Assert.IsTrue(helpText.Contains("version"));
+        }
+
+        [TestMethod]
+        public void TestHelpGeneration_WithConfig() {
+            var getopt = new GetOpt {
+                Options = new Option[] {
+                    new Option("help", ArgumentType.None, 'h', "Displays this help text."),
+                    new Option("version", ArgumentType.None, 'v', "Displays the version of this program.")
+                }
+            };
+
+            var helpText = getopt.GenerateHelpText(new HelpTextConfig {
+                ApplicationName = "getopt.net reference",
+                ApplicationVersion = "1.0.0",
+                FooterText = "This is a footer text."
+            });
+
+            Assert.IsNotNull(helpText);
+            Assert.IsTrue(helpText.Contains("help"));
+            Assert.IsTrue(helpText.Contains("version"));
+            Assert.IsTrue(helpText.Contains("getopt.net reference"));
+            Assert.IsTrue(helpText.Contains("1.0.0"));
+            Assert.IsTrue(helpText.Contains("This is a footer text."));
+        }
+
+        [TestMethod]
+        public void TestHelpGeneration_WithCopyright() {
+            var getopt = new GetOpt {
+                Options = new Option[] {
+                    new Option("help", ArgumentType.None, 'h', "Displays this help text."),
+                    new Option("version", ArgumentType.None, 'v', "Displays the version of this program.")
+                }
+            };
+
+            var helpText = getopt.GenerateHelpText(new HelpTextConfig {
+                ApplicationName = "getopt.net reference",
+                ApplicationVersion = "1.0.0",
+                FooterText = "This is a footer text.",
+                CopyrightDate = new DateTime(2021, 1, 1),
+                CopyrightHolder = "Simon Cahill"
+            });
+
+            Assert.IsNotNull(helpText);
+            Assert.IsTrue(helpText.Contains("help"));
+            Assert.IsTrue(helpText.Contains("version"));
+            Assert.IsTrue(helpText.Contains("getopt.net reference"));
+            Assert.IsTrue(helpText.Contains("1.0.0"));
+            Assert.IsTrue(helpText.Contains("This is a footer text."));
+            Assert.IsTrue(helpText.Contains("©"));
+            Assert.IsTrue(helpText.Contains("2021"));
+            Assert.IsTrue(helpText.Contains("Simon Cahill"));
+        }
+
+        [TestMethod]
+        public void TestHelpGeneration_WithConfig_WithCopyright_WithWindowsConvention() {
+            var getopt = new GetOpt {
+                Options = new Option[] {
+                    new Option("help", ArgumentType.None, 'h', "Displays this help text."),
+                    new Option("version", ArgumentType.None, 'v', "Displays the version of this program.")
+                }
+            };
+
+            var helpText = getopt.GenerateHelpText(new HelpTextConfig {
+                ApplicationName = "getopt.net reference",
+                ApplicationVersion = "1.0.0",
+                FooterText = "This is a footer text.",
+                OptionConvention = OptionConvention.Windows,
+                ShowSupportedConventions = true,
+                CopyrightDate = new DateTime(2021, 1, 1),
+                CopyrightHolder = "Simon Cahill"
+            });
+
+            Assert.IsNotNull(helpText);
+            Assert.IsTrue(helpText.Contains("/help"));
+            Assert.IsTrue(helpText.Contains("/version"));
+            Assert.IsTrue(helpText.Contains("/h"));
+            Assert.IsTrue(helpText.Contains("/v"));
+            Assert.IsTrue(helpText.Contains("/h, /help"));
+            Assert.IsTrue(helpText.Contains("/v, /version"));
+            Assert.IsTrue(helpText.Contains("getopt.net reference"));
+            Assert.IsTrue(helpText.Contains("1.0.0"));
+            Assert.IsTrue(helpText.Contains("This is a footer text."));
+            Assert.IsTrue(helpText.Contains("©"));
+            Assert.IsTrue(helpText.Contains("2021"));
+            Assert.IsTrue(helpText.Contains("Simon Cahill"));
+        }
+
+        [TestMethod]
+        public void TestHelpGeneration_WithConfig_WithCopyright_WithPowershellConvention() {
+            var getopt = new GetOpt {
+                Options = new Option[] {
+                    new Option("help", ArgumentType.None, 'h', "Displays this help text."),
+                    new Option("version", ArgumentType.None, 'v', "Displays the version of this program.")
+                }
+            };
+
+            var helpText = getopt.GenerateHelpText(new HelpTextConfig {
+                ApplicationName = "getopt.net reference",
+                ApplicationVersion = "1.0.0",
+                FooterText = "This is a footer text.",
+                OptionConvention = OptionConvention.Powershell,
+                ShowSupportedConventions = true,
+                CopyrightDate = new DateTime(2021, 1, 1),
+                CopyrightHolder = "Simon Cahill"
+            });
+
+            Assert.IsNotNull(helpText);
+            Assert.IsTrue(helpText.Contains("-help"));
+            Assert.IsTrue(helpText.Contains("-version"));
+            Assert.IsTrue(helpText.Contains("-h"));
+            Assert.IsTrue(helpText.Contains("-v"));
+            Assert.IsTrue(helpText.Contains("-h, -help"));
+            Assert.IsTrue(helpText.Contains("-v, -version"));
+            Assert.IsTrue(helpText.Contains("getopt.net reference"));
+            Assert.IsTrue(helpText.Contains("1.0.0"));
+            Assert.IsTrue(helpText.Contains("This is a footer text."));
+            Assert.IsTrue(helpText.Contains("©"));
+            Assert.IsTrue(helpText.Contains("2021"));
+            Assert.IsTrue(helpText.Contains("Simon Cahill"));
         }
 
     }
